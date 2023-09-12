6c36466a32ca4ac404d5a2b3865e3baaca6ad341
R. Brink <gitlab@rbrink.nl>
2023-09-12 23:37:27 +0200

    * Coding enhancements.

---
+++ src/NetActive.CleanArchitecture.Application.EntityFrameworkCore/Services/BaseEntityQueryService.cs
@@ -0,0 +1,32 @@
+﻿namespace NetActive.CleanArchitecture.Application.EntityFrameworkCore.Services
+{
+    using System.Linq.Expressions;
+    
+    using NetActive.CleanArchitecture.Domain.Interfaces;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public abstract class BaseEntityQueryService<TEntity, TKey>
+        where TEntity : class, IEntity<TKey>, IAggregateRoot
+        where TKey : struct
+    {
+        private IRepository<TEntity, TKey> _repo;
+
+        internal BaseEntityQueryService(IRepository<TEntity, TKey> repo)
+        {
+            _repo = repo ?? throw new ArgumentNullException(nameof(repo));
+        }
+
+        internal IQueryable<TEntity> getQuery(Expression<Func<TEntity, bool>>? where, string[]? includes = null)
+        {
+            var query = _repo.All(includes).AsNoTracking();
+
+            if (where != null)
+            {
+                query = query.Where(where);
+            }
+
+            return query;
+        }
+    }
+}
