64d915fcfb228457ec77c20f07065cdc2a7ec45c
Raymond Brink <github@rbrink.nl>
2024-12-13 15:59:28 +0100

    - Removed unused file.

--- examples/Example.Application/AssemblyReference.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Example.Application
-{
-    using System.Reflection;
-
-    public static class AssemblyReference
-    {
-        public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
-    }
-}
